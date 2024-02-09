8dd7a8b242027b0073352106ca026a82aea986e3
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-02-09 08:53:20 +0100

    core app template setup

--- project/urls.py
+++ project/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("base", views.base, name="base"),
+    path("", views.home, name="home"),
+    path("course/single", views.course_single, name="course_single"),
+    path("course/list", views.course_list, name="course_list"),
+    path("contact", views.contact_us, name="contact_us"),
+    path("about", views.about_us, name="about_us"),
+
+    
 ]
--- project/views.py
+++ project/views.py
@@ -3,3 +3,21 @@ from django.urls import path
 
 def base(request):
     return render(request, "base.html")
+
+def home(request):
+    return render(request, "core/home.html")
+
+def course_single(request):
+    return render(request, "core/course_single.html")
+
+
+def course_list(request):
+    return render(request, "core/course_list.html")
+
+
+def contact_us(request):
+    return render(request, "core/contact_us.html")
+
+
+def about_us(request):
+    return render(request, "core/about_us.html")
